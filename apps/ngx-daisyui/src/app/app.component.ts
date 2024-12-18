34820142b958a41c1ff410df6c2c23597f10a4ed
eurusik <john.rusakov@gmail.com>
2024-12-18 23:13:57 +0200

    feat: added ng-doc as docs portal

--- apps/ngx-daisyui/src/app/app.component.ts
+++ apps/ngx-daisyui/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgDocNavbarComponent, NgDocRootComponent, NgDocSidebarComponent } from '@ng-doc/app';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
