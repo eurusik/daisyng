42e736f2b4cd963b1de8371e50366d02fb8eecec
eurusik <john.rusakov@gmail.com>
2024-12-26 18:21:09 +0200

    docs: add ts doc to button token

--- kit/directives/button/src/button.token.ts
+++ kit/directives/button/src/button.token.ts
@@ -1,8 +1,18 @@
 import { inject, InjectionToken } from '@angular/core';
 import { ButtonConfig, defaultConfig } from './button.config';
 
+/**
+ * Injection token for the button configuration.
+ * Used to inject the `ButtonConfig` into components or services.
+ */
 export const ButtonConfigToken = new InjectionToken<ButtonConfig>('ButtonConfig');
 
+/**
+ * Function to inject the button configuration.
+ * If the `ButtonConfig` is not provided, it returns the default configuration.
+ *
+ * @returns The injected `ButtonConfig` or the `defaultConfig` if not provided.
+ */
 export function injectButtonConfig(): ButtonConfig {
   return inject(ButtonConfigToken, { optional: true }) ?? defaultConfig;
 }
