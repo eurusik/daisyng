8ce7ce891e7c38eb484c0cdb56d958fb5b2a50e7
eurusik <john.rusakov@gmail.com>
2024-12-26 18:30:02 +0200

    docs: update for button directive

--- kit/directives/button/src/button-shape.type.ts
+++ kit/directives/button/src/button-shape.type.ts
@@ -1,6 +1,13 @@
 import { ButtonVariants } from './button.config';
 
 /**
- * Type defining the shape of the button.
+ * Type representing the shape of a button.
+ *
+ * This type is derived from the `shape` property of the `ButtonVariants` type.
+ * It allows you to set one of the following values for the shape of the button:
+ *
+ * - `default`: The default shape of the button, usually rectangular with rounded corners.
+ * - `square`: A square-shaped button with no rounded corners.
+ * - `circle`: A circular button, typically with equal width and height.
  */
 export type ButtonShape = ButtonVariants['shape'];
--- kit/directives/button/src/button.config.ts
+++ kit/directives/button/src/button.config.ts
@@ -1,5 +1,8 @@
 import { cva, VariantProps } from 'class-variance-authority';
 
+/**
+ * Configuration for the button styles.
+ */
 export const buttonConfig = cva(
   '',
   {
@@ -23,13 +26,26 @@ export const buttonConfig = cva(
     },
   },
 );
+
+/**
+ * Type representing the possible variants for a button configuration.
+ *
+ * The `ButtonVariants` type is extracted from the `buttonConfig` using `VariantProps`.
+ * This allows for dynamic access to the valid variant keys for the `shape` and `responsive` properties.
+ */
 export type ButtonVariants = VariantProps<typeof buttonConfig>;
 
+/**
+ * Interface representing the structure of a button configuration.
+ */
 export interface ButtonConfig {
   shape: ButtonVariants['shape'];
   responsive: ButtonVariants['responsive'];
 }
 
+/**
+ * Default configuration for the button.
+ */
 export const defaultConfig: ButtonConfig = {
   shape: 'default',
   responsive: 'default'
