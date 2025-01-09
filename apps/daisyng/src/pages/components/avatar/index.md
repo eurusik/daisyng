25611a2735f1dd0060f18f539098f145c6c0f502
eurusik <john.rusakov@gmail.com>
2025-01-09 18:53:30 +0200

    docs: update avatar

--- apps/daisyng/src/pages/components/avatar/index.md
+++ apps/daisyng/src/pages/components/avatar/index.md
@@ -14,28 +14,90 @@ import { AvatarComponent } from '@daisyng/kit/components/avatar';
 
 ### Avatar in custom sizes
 
+```html
+<dsy-avatar size="medium">
+  <img src="/avatar.png" alt="avatar" />
+</dsy-avatar>
+```
+
+- Sizes: `tiny`, `small`, `medium`, `large`.
+
 {{ NgDocActions.demo("SampleAvatarSizeComponent") }}
 
 ### Avatar rounded
+
+```html
+<dsy-avatar rounded="xs">
+  <img src="/avatar.png" alt="avatar" />
+</dsy-avatar>
+```
+
+- Rounded: `xs`, `full`. 
 
 {{ NgDocActions.demo("SampleAvatarRoundedComponent") }}
 
 ### Avatar with ring
 
+```html
+<dsy-avatar ring="primary">
+  <img src="/avatar.png" alt="avatar" />
+</dsy-avatar>
+```
+
+- Ring variants: `primary`, `secondary`, `neutral`, `accent`, `info`, `success`, `warning`, `error`. 
+
 {{ NgDocActions.demo("SampleAvatarWithRingComponent") }}
 
 ### Avatar group
+
+```html
+<dsy-avatar-group>
+  <dsy-avatar>
+    <img src="/avatar.png" alt="avatar" />
+  </dsy-avatar>
+  ...
+</dsy-avatar-group>
+```
 
 {{ NgDocActions.demo("SampleAvatarGroupComponent") }}
 
 ### Avatar group with counter
 
+```html
+<dsy-avatar-group>
+  <dsy-avatar>
+    <img src="/avatar.png" alt="avatar" />
+  </dsy-avatar>
+  ...
+  <dsy-avatar [placeholder]="true">
+    <span class="text-xs">+99</span>
+  </dsy-avatar>
+</dsy-avatar-group>
+```
+
 {{ NgDocActions.demo("SampleAvatarGroupWithCounterComponent") }}
 
 ### Avatar with presence indicator
 
+```html
+<dsy-avatar indicator="online">
+  <img src="/avatar.png" alt="avatar" />
+</dsy-avatar>
+```
+
+- States: `online`, `offline`. 
+
 {{ NgDocActions.demo("SampleAvatarIndicatorComponent") }}
 
 ### Avatar placeholder
+
+```html
+<dsy-avatar [placeholder]="true">
+  <span class="text-3xl">D</span>
+</dsy-avatar>
+```
+
+- Shapes: `square`, `circle`. 
+- Types: `primary`, `secondary`, `neutral`, `accent`, `info`, `success`, `warning`, `error`.
 
 {{ NgDocActions.demo("SampleAvatarPlaceholderComponent") }}
--- kit/components/avatar/src/avatar.component.ts
+++ kit/components/avatar/src/avatar.component.ts
@@ -15,6 +15,10 @@ import {
   AvatarSizeDirective,
 } from '@daisyng/kit/directives/avatar';
 
+/**
+ * Represents an avatar component that can display images, placeholders, and indicators with various styles and configurations.
+ * This component is designed to be flexible, allowing for a wide range of avatar appearances through input properties.
+ */
 @Component({
   selector: 'dsy-avatar',
   imports: [
@@ -32,31 +36,53 @@ import {
   },
 })
 export class AvatarComponent {
+  /** Determines whether the avatar is a placeholder. */
   readonly placeholder: InputSignal<boolean> = input<boolean>(false);
 
+  /** Specifies the rounded style for the placeholder if `placeholder` is true. */
   readonly placeholderRounded: InputSignal<AvatarPlaceholderRounded> =
     input<AvatarPlaceholderRounded>(null);
 
+  /** Specifies the type of the placeholder if `placeholder` is true. */
   readonly placeholderType: InputSignal<AvatarPlaceholderType> =
     input<AvatarPlaceholderType>(null);
 
+  /** Defines the border-radius of the avatar. */
   readonly rounded: InputSignal<AvatarRounded> = input<AvatarRounded>('xs');
 
+  /** Sets the size of the avatar. */
   readonly size: InputSignal<AvatarSize> = input<AvatarSize>('medium');
 
+  /** Applies a ring effect around the avatar. */
   readonly ring: InputSignal<AvatarRing> = input<AvatarRing>(null);
 
+  /** Displays an indicator, such as online or offline status, next to the avatar. */
   readonly indicator: InputSignal<AvatarIndicator> =
     input<AvatarIndicator>(null);
 
+  /**
+   * Computed property that determines the rounded style for the placeholder.
+   * Returns 'circle' as the default rounded style if the avatar is a placeholder.
+   * @internal
+   */
   readonly phRounded = computed(() =>
     this.placeholder() ? this.placeholderRounded() || 'circle' : null
   );
 
+  /**
+   * Computed property that determines the type for the placeholder.
+   * Returns 'neutral' as the default type if the avatar is a placeholder.
+   * @internal
+   */
   readonly phType = computed(() =>
     this.placeholder() ? this.placeholderType() || 'neutral' : null
   );
 
+  /**
+   * Computed property that generates the class string based on the avatar's configuration.
+   * It merges custom styles with the default configuration for indicators and placeholders.
+   * @internal
+   */
   readonly classes = computed(() =>
     classMerge(
       avatarConfig({
